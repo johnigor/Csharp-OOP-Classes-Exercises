e0dd45171743f72c5c4e9cbab7038622c937ce37
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-18 01:05:07 -0300

    TimeSpan e DateTime

--- TopicosEspeciaisC-Sharp/DateTime/DateTime/Program.cs
+++ TopicosEspeciaisC-Sharp/DateTime/DateTime/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Globalization;
 
-namespace DateTime
+namespace DateTime1
 {
     internal class Program
     {
@@ -10,7 +11,64 @@ namespace DateTime
             //É um tipo valor (struct)
 
             /* Um objeto DateTime internamente armazena: 
-             O número de "ticks" (100 nanosegundos) desde a meia noite do dia 1 de janeiro do ano 1 da era comum. */
+             O número de "ticks" (100 nanosegundos) desde a meia noite do dia 1 de janeiro do ano 1 da era comum. 
+            
+             Construtores: 
+            DateTime(ano, mes, dia)
+            DateTime(ano, mes, dia, hora, minuto, segundo) [opcional:kind]
+            DateTime(ano, mes, dia, hora, minuto, segundo, milissegundo) [opcional:kind]
+
+             Builders: Operações ou propriedades que vão instanciar para você um DateTime
+            DateTime.Now
+            DateTime.UtcNow
+            DateTime.Today
+            DateTime.Parse(string)
+            DateTime.ParseExact(string, string)
+            */
+
+
+            //Mostra a data e horario de agora (o momento em que você está)
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            //Imprime a quantidade de ticks que já passaram desde o dia 1 de janeiro 00:00 da era atual, até o dia de hoje
+            Console.WriteLine(d1.Ticks);
+
+            DateTime d2 = new DateTime(2018, 11, 25); //Construtor com ano, mes e dia
+            Console.WriteLine(d2);
+
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3); //ano, mes, dia, hora, minuto e segundo
+            Console.WriteLine(d3);
+
+            DateTime d4 = new DateTime(2018, 11, 25, 20, 45, 3, 500); //ano, mes, dia, hora, minuto, segundo e milissegundo
+            Console.WriteLine(d4);
+
+            DateTime d5 = DateTime.Today; //Data de hoje porém com o horário zerado
+            Console.WriteLine(d5);
+
+            DateTime d6 = DateTime.UtcNow; //Horario de agora, porem no horario universal UTC
+            Console.WriteLine(d6);
+
+            //Utilizando o Parse para converter uma data de string para date time
+
+            DateTime d7 = DateTime.Parse("2000-08-15"); //Ano, mes e dia (formato utilizado por alguns bancos de dados)
+            Console.WriteLine(d7);
+            DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+            Console.WriteLine(d8);
+
+            DateTime d9 = DateTime.Parse("15/08/2000"); //Formato brasileiro
+            Console.WriteLine(d9);
+            DateTime d10 = DateTime.Parse("15/08/2000 13:05:58");
+            Console.WriteLine(d10);
+
+            //Parse Exact - Para voce determinar o formato da sua data
+
+            DateTime d11 = DateTime.ParseExact("2000-08-15","yyyy-MM-dd", CultureInfo.InvariantCulture); //Mascara de formatacao
+            Console.WriteLine(d11);
+
+            DateTime d12 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            Console.WriteLine(d12);
+
+
         }
     }
 }
---
+++ TopicosEspeciaisC-Sharp/TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TimeSpan1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Representa uma duração
+            //Tipo valor (struct)
+
+            /* Um objeto TimeSpan internamente armazena uma duração na forma de ticks (100 nanosegundos)*/
+
+            TimeSpan t1 = new TimeSpan(0, 1, 30); //construtor de 3 argumentos (hora, minuto, segundo)
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+
+            TimeSpan t2 = new TimeSpan(); //Construtor padrão, vazio. Cria um TimeSpan com a duração 0
+            Console.WriteLine(t2);
+            
+            TimeSpan t3 = new TimeSpan(900000000L); //Construtor informando a quantidade de ticks (o "L" no final significa LONG)
+            Console.WriteLine(t3);
+
+            TimeSpan t4 = new TimeSpan(2, 11, 21); //Horas minutos segundos
+            Console.WriteLine(t4);
+
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21); //Dia, hora, minuto, segundo
+            Console.WriteLine(t5);
+
+            TimeSpan t6 = new TimeSpan(1, 2, 11, 21, 321); //Dia, hora, minuto, segundo, milisegundos
+
+            //Metodos From
+            //Cria um TimeSpan equivalente a 1.5 dias
+            TimeSpan t7 = TimeSpan.FromDays(1.5); 
+            Console.WriteLine(t7);
+
+            //TimeSpan de uma hora e meia
+            TimeSpan t8 = TimeSpan.FromHours(1.5);
+            Console.WriteLine(t8);
+
+            //TimeSpan de um minuto e meio
+            TimeSpan t9 = TimeSpan.FromMinutes(1.5);
+            Console.WriteLine(t9);
+
+            //TimeSpan de um segundo e meio
+            TimeSpan t10 = TimeSpan.FromSeconds(1.5);
+            Console.WriteLine(t10);
+
+            //TimeSpan de um e meio milisegundos
+            TimeSpan t11 = TimeSpan.FromMilliseconds(1.5);
+            Console.WriteLine(t11);
+
+            //TimeSpan de ticks
+            TimeSpan t12 = TimeSpan.FromMinutes(900000000L);
+            Console.WriteLine(t12);
+        }
+    }
+}
