3a56a5f212162ec02a19ad87d67167f5e4223b83
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-24 20:55:24 -0300

    herança

---
+++ Heranca-Polimorfismo/Heranca/Heranca/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Herança: 
+            - É um tipo de associação que permite que uma classe herde dados e comportamentos de outra
+            
+            Definições importantes:
+            - Vantagens
+                Reuso
+                Polimorfismo
+            
+            - Sintaxe
+                : (estende)
+                base (referência para a superclasse)
+
+            - Relação "é-um"
+            - Generalização / Especialização
+            - Superclasse (classe base) / subclasse (classe derivada)
+            - Herança / Extensão
+            - Herança é uma associação entre classes e não entre objetos
+            */
+
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
---
+++ Heranca-Polimorfismo/UpCasting-e-DownCasting/UpCasting-e-DownCasting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UpCasting_e_DownCasting.Entities;
+namespace UpCasting_e_DownCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Upcasting:
+            - Casting da subclasse para superclasse
+            - Uso comum: polimorfismo
+
+            Downcasting:
+            - Casting da superclasse para subclasse
+            - Palavra as
+            - Palavra is
+            - Uso comum: métodos que recebem parâmetros genéricos (ex: Equals)
+            */
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500);
+
+            //UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            //DOWNCASTING
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+            // acc2.Loan(100.0); Sem o downcasting vai dar falha na compilação
+
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+        }
+    }
+}
