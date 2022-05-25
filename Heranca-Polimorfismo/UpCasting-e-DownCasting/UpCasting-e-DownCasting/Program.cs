b5a4796621bd62d668631a46bfa938961a2bbc32
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-24 21:01:22 -0300

    upcasting e downcasting

--- Heranca-Polimorfismo/UpCasting-e-DownCasting/UpCasting-e-DownCasting/Program.cs
+++ Heranca-Polimorfismo/UpCasting-e-DownCasting/UpCasting-e-DownCasting/Program.cs
@@ -27,11 +27,26 @@ namespace UpCasting_e_DownCasting
             Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
 
             //DOWNCASTING
+            //Usar só quando necessário, testar se a variável é do tipo que você quer
             BusinessAccount acc4 = (BusinessAccount)acc2;
             acc4.Loan(100.0);
             // acc2.Loan(100.0); Sem o downcasting vai dar falha na compilação
 
-            BusinessAccount acc5 = (BusinessAccount)acc3;
+            //BusinessAccount acc5 = (BusinessAccount)acc3; //acc3 é "savingsaccount", o compilador só percebe na hora de debugar
+
+            if (acc3 is BusinessAccount) //false
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount; //Outra forma de fazer o casting
+            }
+
+            if (acc3 is SavingsAccount) //true
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
