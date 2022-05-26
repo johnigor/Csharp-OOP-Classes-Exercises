a3dd216784b554918743a773aa15e9cfddc800e4
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-26 11:00:32 -0300

    desafio tratamento de excecoes

---
+++ TratamentoDeExcecoes/DesafioTratamentoExcecoes/DesafioTratamentoExcecoes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using DesafioTratamentoExcecoes.Entities;
+using DesafioTratamentoExcecoes.Entities.Exceptions;
+
+namespace DesafioTratamentoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data:");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, initialBalance, withdrawLimit);
+            Console.WriteLine();
+
+            try
+            {
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(amount);
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+    }
+}
