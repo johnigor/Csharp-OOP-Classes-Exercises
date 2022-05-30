7c1ac6221e68aca79eecd7dad66270be60fee3fd
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-30 10:37:58 -0300

    FileStream, StreamReader, StreamWriter

---
+++ TrabalhandoComArquivos/BlocoUsing/BlocoUsing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace BlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Using Block:
+
+            - Sintaxe simplificada que garante que os objetos IDispoasble serão fechados.
+            - Objetos IDisposable NÃO são gerenciados pelo clr. Eles precisam ser manualmente fechados.
+                Exemplos: Font, FileStream, StreamReader, StreamWriter
+            */
+
+            string path = @"C:\temp\file1.txt";
+
+            //Tudo deste bloco será executado e ao final dele finalizado
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
---
+++ TrabalhandoComArquivos/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace _StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Stream Writer:
+
+            - É uma stream capaz de escrever caracteres a partir de uma stream binária (ex: FileStream).
+            - Suporte a dados no formato de texto.
+            
+                Instantiation:
+            - Several constructors
+            - File / FileInfo
+                *CreateText(path)
+                *AppendText(String)
+            */
+
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
