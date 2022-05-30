8d45104c6aafffd7b134a74590b3df3a4d550bb6
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-30 11:11:07 -0300

    Directory, DirectoryInfo e Path

---
+++ TrabalhandoComArquivos/Directory-DirectoryInfo/Directory-DirectoryInfo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Directory_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Directory, DirectoryInfo:
+
+            - Namespace System.IO
+            - Operações com pastas (create, enumerate, get files, etc.)
+            
+            - Directory:
+                * static members (simple, but performs security check for each operation)
+            - DirectoryInfo:
+                * instance members
+            */
+
+            string path = @"C:\temp\myfolder";
+
+            try
+            {
+                //Teste para listar todas as subpastas a partir da pasta "myfolder"
+                //Para resumir a declaração "IEnumerable<string>", pode-se usar a palavara "var"
+                // var folders = ...
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //Listar todos os arquivos a partir de uma pasta
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //Criar uma pasta
+                Directory.CreateDirectory(path + "\\newfolder"); //Por ser string comum, deve-se usar duas barras ou então utilizar @"\
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ TrabalhandoComArquivos/Path/Path/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Path (caminho):
+            
+            - Namespace System.IO
+            - Realiza operações com strings que contém informações de arquivos ou pastas.
+            */
+
+            string path = @"c:\temp\myfolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path)); //nome da pasta
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path)); //nome do arquivo
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath()); //pasta temporária do sistema
+        }
+    }
+}
