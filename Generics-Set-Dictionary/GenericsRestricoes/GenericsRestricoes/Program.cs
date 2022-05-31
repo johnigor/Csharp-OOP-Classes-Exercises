a07e6102fe0ec4beb443a0143008c2319a514d19
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-31 14:58:24 -0300

    Generics, aula sobre restrições

---
+++ Generics-Set-Dictionary/GenericsRestricoes/GenericsRestricoes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using GenericsRestricoes.Entities;
+using GenericsRestricoes.Services;
+
+namespace GenericsRestricoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Most expensive:");
+            Console.WriteLine(max);
+        }
+    }
+}
+
+/*
+ Restrições possíveis
+
+- where T: struct
+- where T: class
+- where T: unmanaged
+- where T: new()
+- where T: <base type name>
+- where T: U
+ */ 
---
+++ Generics-Set-Dictionary/IntroGenerics/Aula_213_ex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                printService.AddValue(number);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
