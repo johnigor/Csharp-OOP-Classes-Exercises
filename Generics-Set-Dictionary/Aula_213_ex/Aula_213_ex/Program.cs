4ffeb8e421117c0634f8884f09361998eadc8423
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-05-31 12:01:59 -0300

    Iniciando Generics

---
+++ Generics-Set-Dictionary/Aula_213_ex/Aula_213_ex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int N = int.Parse(Console.ReadLine());            
+
+            for (int i = 0; i < N; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                printService.AddValue(number);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First()); 
+        }
+    }
+}
