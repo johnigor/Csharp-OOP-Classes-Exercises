53ca5f5c5c1cb74d72cf071fd951dd855145fd54
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-13 16:23:52 -0300

    Aula sobre LINQ
    Resumo da aula:
    Operacoes: Where, Select, OrderBy, ThenBy, Skip, Take, Firstm FirstOrDefault, Single, SingleOrDefault,, Max, Min, Sum, GroupBy, Average e Aggregate

---
+++ Lambda-Delegates-LINQ/IntroducaoLINQ/IntroducaoLINQ/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroducaoLINQ.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+    }
+}
