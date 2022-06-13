cf1ea2ee6d327956549ffa2a2c2e48d5668b4714
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-13 14:14:20 -0300

    introducao ao linq

---
+++ Lambda-Delegates-LINQ/IntroducaoLINQ/IntroducaoLINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace IntroducaoLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //specifying the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
