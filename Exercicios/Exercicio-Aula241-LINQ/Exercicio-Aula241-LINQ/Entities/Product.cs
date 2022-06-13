0e8c4a6050d864d277df38ef634bbf22f768c812
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-13 17:55:40 -0300

    Exercicio LINQ

---
+++ Exercicios/Exercicio-Aula241-LINQ/Exercicio-Aula241-LINQ/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio_Aula241_LINQ.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
