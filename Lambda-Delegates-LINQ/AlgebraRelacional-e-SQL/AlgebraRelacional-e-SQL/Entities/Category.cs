0bccc61a53a44af5d73d95d67f39d33878ecaa1a
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-13 16:53:42 -0300

    Algebra relacional e SQL

---
+++ Lambda-Delegates-LINQ/AlgebraRelacional-e-SQL/AlgebraRelacional-e-SQL/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace AlgebraRelacional_e_SQL.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ Lambda-Delegates-LINQ/AlgebraRelacional-e-SQL/AlgebraRelacional-e-SQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgebraRelacional_e_SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
