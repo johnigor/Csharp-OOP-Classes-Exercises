1bd4d1419252566f0a2374401d2b3a9050149348
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-06-14 11:08:42 -0300

    Exercicio de fixacao LINQ

---
+++ Exercicios/ExFixacao-LINQ/ExFixacao-LINQ/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using ExFixacao_LINQ.Entities;
+using System.Collections.Generic;
+
+namespace ExFixacao_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            Console.Write("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        employees.Add(new Employee(name, email, salary));
+                    }
+
+                    var emails = employees.Where(obj => obj.Salary > limit).OrderBy(obj => obj.Email).Select(obj => obj.Email);
+
+                    var sum = employees.Where(obj => obj.Name.StartsWith('M')).Sum(obj => obj.Salary);
+
+                    Console.WriteLine("Email of people whose salary is more than: " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                    foreach (string email in emails)
+                    {
+                        Console.WriteLine(email);
+                    }
+
+                    Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
