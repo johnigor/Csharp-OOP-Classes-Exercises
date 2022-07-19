fc5eb1bab731a198e6be906ca81eeb24ce630275
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-07-19 12:32:56 -0300

    cache + ex fixacao

---
+++ Exercicios/ExPagamentos/teste/Entities/Funcionario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PagamentosFuncionario.Entities
+{
+    class Funcionario
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public int DiasDeTrabalho { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario()
+        {
+        }
+        public Funcionario(string cpf, string nome, int diasDeTrabalho, double salario)
+        {
+            Cpf = cpf;
+            Nome = nome;
+            DiasDeTrabalho = diasDeTrabalho;
+            Salario = salario;
+        }
+        public int Meses()
+        {
+            int meses = 0;
+            int dias = DiasDeTrabalho;
+
+            if (dias <= 31) meses = 1;
+            if (dias > 31 && dias <= 59) meses = 2;
+            if (dias > 59 && dias <= 90) meses = 3;
+            if (dias > 90 && dias <= 120) meses = 4;
+            if (dias > 120 && dias <= 151) meses = 5;
+            if (dias > 151 && dias <= 181) meses = 6;
+            if (dias > 181 && dias <= 212) meses = 7;
+            if (dias > 212 && dias <= 243) meses = 8;
+            if (dias > 243 && dias <= 273) meses = 9;
+            if (dias > 273 && dias <= 304) meses = 10;
+            if (dias > 304 && dias <= 334) meses = 11;
+            if (dias > 334 && dias <= 365) meses = 11;
+
+            return meses;
+        }
+
+        public double Anos()
+        {
+            double anos = DiasDeTrabalho / 365.0;
+            return anos;
+        }
+
+        public double Inss(double aliquota)
+        {
+            aliquota = 0;
+            if (Salario <= 1100) aliquota = 0.075; //7,5%
+            if (Salario > 1212 && Salario < 2427) aliquota = 0.09; //9%
+            if (Salario > 2203 && Salario < 3305) aliquota = 0.12; //12%
+            if (Salario > 3305 && Salario < 6433) aliquota = 0.14; //14%
+
+            return aliquota;
+        }
+        public double PagamentoMensal()
+        {
+            double pagamento = Salario - ((Salario * 0.06) + (Salario * Inss(Salario)));
+            return pagamento;
+        }
+    }
+}
---
+++ Exercicios/ExPagamentos/teste/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using PagamentosFuncionario.Entities;
+
+namespace PagamentosFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            Console.WriteLine("Bem vindo, John!");
+            Console.WriteLine();
+            Console.Write("Deseja cadastrar um funcionário? y/n: ");
+            string resposta = Console.ReadLine();
+            Console.WriteLine();
+
+            if (resposta.ToUpper().Equals("Y"))
+            {
+                do
+                {
+                    Console.Write("Digite o CPF do funcionário: ");
+                    string cpf = Console.ReadLine();
+
+                    Console.Write("Qual o nome do funcionário? ");
+                    string nome = Console.ReadLine();
+
+                    int diasDeTrabalho = 0;
+
+                    Console.Write("Informe o salário do funcionário: ");
+                    double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.WriteLine();
+
+                    Funcionario funcionario = new Funcionario()
+                    {
+                        Cpf = cpf,
+                        Nome = nome,
+                        DiasDeTrabalho = diasDeTrabalho,
+                        Salario = salario
+                    };
+                    funcionarios.Add(funcionario);
+
+                    Console.WriteLine("Deseja cadastrar outro funcionário? y/n: ");
+                    resposta = Console.ReadLine();
+                    Console.WriteLine();
+                }
+                while (resposta.ToUpper().Equals("Y"));
+
+                Console.WriteLine();
+
+                Console.Write("Deseja fazer consultas para algum funcionário? y/n: ");
+
+                resposta = Console.ReadLine();
+
+                if (resposta.ToUpper().Equals("Y"))
+                {
+                    Console.Write("Digite o Cpf do funcionário a ser consultado: ");
+                    string cpf = Console.ReadLine();
+                    Console.WriteLine("Selecione a consulta que deseja fazer: ");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Dias trabalhados: ");
+                    Console.WriteLine("Salário ");
+                    Console.WriteLine();
+                }
+
+            }
+        }
+    }
+}
